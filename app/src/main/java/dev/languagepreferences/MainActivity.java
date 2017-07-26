68aa8f5564b9fc221d71582468b6a69c7e470a08
Ozols <its27eo1@odin.local>
2017-07-26 15:33:26 +0300

    Latvenergo VRT vectors and no callback vector completed

--- app/src/main/java/dev/languagepreferences/MainActivity.java
+++ app/src/main/java/dev/languagepreferences/MainActivity.java
@@ -1,7 +1,10 @@
 package dev.languagepreferences;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        new AlertDialog.Builder(this)
+                .setIcon(android.R.drawable.ic_menu_compass)
+                .setTitle("Choose a language")
+                .setMessage("Which languange would you like?")
+                .setPositiveButton("English", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+
+                        Toast.makeText(MainActivity.this, "English", Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .setPositiveButton("Spanish", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+
+                        Toast.makeText(MainActivity.this, "Spanish", Toast.LENGTH_SHORT).show();
+                    }
+                });
     }
 }
